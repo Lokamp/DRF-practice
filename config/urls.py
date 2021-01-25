03dd394714a9fa490370fffde747d2851ed9ee47
Evgen <sokolov.tt@gmail.com>
2021-01-25 14:32:29 +0300

    refactoring

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-api_url = [
+api_urlpatterns = [
     path('items/', include('items.urls')),
     path('reviews/', include('reviews.urls')),
     path('users/', include('users.urls'))
